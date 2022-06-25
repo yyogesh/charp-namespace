edcfb0b4fcf61403320737e75baefa5ce70efdcf
yyogesh <yogeshggn1@gmail.com>
2022-06-25 15:01:02 +0530

    namespace

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using MyNewApp.Utilities;
+
+namespace MyNewApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = Utility.CalculateAge(new DateTime(2002, 7, 25));
+            Console.WriteLine($"age is {age}");
+        }
+    }
+}
+
+namespace MyNewApp.Utilities
+{
+    class Utility
+    {
+        public static int CalculateAge(DateTime dateOfBirth)
+        {
+            int age = 0;
+            age = DateTime.Now.Year - dateOfBirth.Year;
+            return age;
+        }
+    }
+}
